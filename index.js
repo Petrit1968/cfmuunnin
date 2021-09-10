ba03fa3ec733090ab927f70fabd9f2cf00abcffb
Taipale <Petri.Taipale@seamk.fi>
2021-09-10 15:39:46 +0300

    Hei Hei1

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const input = documen.querySelector('input');
+const fahrenheitTuloste = documen.querySelector('#fahrenheit');
+const celsiusTuloste = documen.querySelector('#celsius');
+const muunnaButton = document.querySelector('button');
+
 function muunna() {
 
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', muunna);
+// const button = document.querySelector('button');
+muunnabutton.addEventListener('click', muunna);
