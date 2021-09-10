6f03b408090e8720eced56cb7ba5341f04d84507
Taipale <Petri.Taipale@seamk.fi>
2021-09-10 16:00:37 +0300

    Hei Hei5

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const fahrenheitTuloste = documen.querySelector('#fahrenheit');
 const celsiusTuloste = documen.querySelector('#celsius');
 const muunnaButton = document.querySelector('button');
 
+function fahrenheitCelsiukseksi(fahrenheit) {
+    let celsius = fahrenheit;
+    return celsius;
+
+}
+
 function muunna() {
     let syote = fahrenheitSyote.value;
     fahrenheitTuloste.textcontent = syote;
